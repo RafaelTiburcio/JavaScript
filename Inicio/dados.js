d7849fadd36e19a7eab504584f9ebb313a068c70
Rafael Tiburcio <108090164+RafaelTiburcio@users.noreply.github.com>
2023-05-10 10:32:23 -0300

    js inicio

---
+++ Inicio/dados.js
@@ -0,0 +1,12 @@
+var quantidade = 10;
+var preco = 6.5;
+var imposto = 3.0;
+var precoFinal = preco + imposto;
+var totalVenda = quantidade * precoFinal;
+
+console.log("Caneta");
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+console.log(totalVenda);
