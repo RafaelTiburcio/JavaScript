b13620ed87c8745093dc8efe7c94d47650d119bb
Rafael Tiburcio <108090164+RafaelTiburcio@users.noreply.github.com>
2023-07-13 10:34:44 -0300

    switch

---
+++ Inicio/JavaScript/repetição/break_continue.js
@@ -0,0 +1,13 @@
+for(let i = 1; i <= 5; i++) {
+    if (i === 3) {
+        break;
+    }
+    console.log(i);
+}
+
+for(let i = 1; i <= 5; i++) {
+    if (i === 3 ) {
+        continue;
+    }
+    console.log(i);
+}
