33348c72c6cf96d7136e33906e06b2bbc9b8da90
Rafael Tiburcio <contatorafaeltiburcio@gmail.com>
2023-09-14 10:49:46 -0300

    Update app.js

--- JavaScript_Intermediario/secao_2/app.js
+++ JavaScript_Intermediario/secao_2/app.js
@@ -8,20 +8,23 @@ const usuario = {
     idade : 24,
     codigoCliente : 90908080,
     valorEmprestimo : 1500,
-    taxaJuros : 0.75,
+    taxaJuros : 7.5,
     valorComJuros : function() {
-         return this.valorEmprestimo + (this.valorEmprestimo * this.taxaJuros);
+         return this.valorEmprestimo + (this.valorEmprestimo * (this.taxaJuros /100));
         },
     bomPagador : "SIM",
 
     exibir() {
-        return `O Cliente ${this.nome}, ID: ${this.codigoCliente}, Solicitou um esmprestimo no valor de R$ ${this.valorEmprestimo},00 com taxa de juros ${this.taxaJuros}% ao ano.
-    Valor a ser pago ao final do emprestimo : R$ ${this.valorComJuros()},00
-    Pagamento em dia: ${this.bomPagador}`
+        return `O Cliente ${this.nome}, ID: ${this.codigoCliente}, Solicitou um esmprestimo no valor de R$ ${this.valorEmprestimo},00 com taxa de juros ${this.taxaJuros}% ao ano.\n
+        Valor a ser pago ao final do emprestimo : R$ ${this.valorComJuros()},00
+        Pagamento em dia: ${this.bomPagador}`
                 
     }
 }
 
-console.log(usuario);
+usuario.nome = "Rafael Tiburcio";
+usuario.valorEmprestimo = 2340;
+usuario.taxaJuros = 50;
+;console.log(usuario);
 console.log(usuario.exibir());
 
