656676304d24615fca8105e9d92ed2a1d098a051
Rafael Tiburcio <contatorafaeltiburcio@gmail.com>
2023-09-28 19:56:22 -0300

    arrays

---
+++ JavaScript_Intermediario/arrays/array1.js
@@ -0,0 +1,15 @@
+const array = ["Elemento0", "Elemento1", "Elemento2"];
+
+const array2 = Array("Elemento0", "Elemento1", "Elemento2");
+
+const array3 = new Array("Elemento0", "Elemento1", "Elemento2");
+
+const frutas = ["Melancia", "Morango", "Laranja", "Abacaxi"];
+frutas.push("Abacate");
+frutas.sort(); // coloca em ordem crescente
+frutas.pop(); // remove ultimo elemento
+frutas.shift(); // remove primeiro elemento
+
+console.log(frutas);
+console.log(array[0]);
+console.log(array[2]);
